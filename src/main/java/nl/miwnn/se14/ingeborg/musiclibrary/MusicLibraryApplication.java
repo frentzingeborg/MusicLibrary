f8199874c128bd0b1c68547279f84f449ab558ab
Ingeborg <frentzingeborg@gmail.com>
2024-10-21 13:41:17 +0200

    Initial commit

---
+++ src/main/java/nl/miwnn/se14/ingeborg/musiclibrary/MusicLibraryApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se14.ingeborg.musiclibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/se14/ingeborg/musiclibrary/MusicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se14.ingeborg.musiclibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
